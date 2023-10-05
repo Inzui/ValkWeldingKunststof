8f29c5fc3eabe17445fa945143cbe6182a3ce819
Jurre van Wamelen <j.vanwamelen@agrocontrol.nl>
2023-10-05 16:41:50 +0200

    Added the controls for the cobot in cobotController.
    Added the controls for the cobot in cobotController.

--- Touch PoC/cobot/cobotController.py
+++ Touch PoC/cobot/cobotController.py
@@ -1,18 +1,75 @@
 class CobotController():
-    def __init__(self, detector):
+    def __init__(self, detector, stepSize = 5):
         self.detector = detector
+        self.hasStarted = False
+        self.stepSize = stepSize
+        self.headPos = [-179, 0, -90]
 
     def start(self):
         print("Starting controller")
-        # Import here as it automatically tries to connect
-        # from cobot.EasyModbusPy.cobotconnect19216801 import cobotconnect
-        # self.cob = cobotconnect()
+        #Import here as it automatically tries to connect
+        from cobot.EasyModbusPy.cobotconnect19216801 import cobotconnect
+        self.cob = cobotconnect()
         self.detector.start()
+        self.hasStarted = True
     
     def stop(self):
-        # self.cob.stop()
+        self.cob.stop()
         self.detector.stop()
+        self.hasStarted = False
         print("Stopped controller")
     
     def detectObject(self):
         print(self.detector.detectObject())
+
+    def moveToSteps(self, point: list, speed: int):
+        if(not self.hasStarted):
+            raise Exception("Cobot has not been started.")
+
+        point += self.headPos
+        point = self._cleanPoint(point)
+        P = self._cleanPoint(self.cob.readPos())
+
+        while not self._arrivedOnPos(P, point):
+            relativeMove = self._getRelativeMove(P, point)
+            self.cob.sendCobotMove(self._toText(relativeMove),speed)
+            P = self._cleanPoint(self.cob.readPos())
+
+    def moveToDirect(self, point: list, speed: int):
+        if(not self.hasStarted):
+            raise Exception("Cobot has not been started.")
+
+        point += self.headPos
+        point = self._cleanPoint(point)
+        self.cob.sendCobotPos(self._toText(point),speed) 
+
+        P = self._cleanPoint(self.cob.readPos())
+
+        while not self._arrivedOnPos(P, point):
+            P = self._cleanPoint(self.cob.readPos())
+
+    def _getRelativeMove(self, currentPos: list, desPos: list) -> list:
+        newPos = [0, 0, 0, 0, 0, 0]
+        for i in range(len(currentPos)):
+            if(abs(currentPos[i] - desPos[i]) < self.stepSize):
+                newPos[i] = currentPos[i] - desPos[i]
+            elif(currentPos[i] < desPos[i]):
+                newPos[i] = -self.stepSize
+            elif(currentPos[i] > desPos[i]):
+                newPos[i] = self.stepSize
+        newPos[0], newPos[1] = newPos[1], newPos[0]
+        return newPos
+
+    def _toText(self, P: list) -> list:
+        return str(P[0]) + " ,"  + str(P[1]) + " ,"  + str(P[2]) + " ,"  + str(P[3]) + " ,"  + str(P[4]) + " ,"  + str(P[5]) 
+
+    def _arrivedOnPos(self, currentPos: list, desPos: list) -> bool:
+        for i in range(len(currentPos)):
+            if(currentPos[i] != desPos[i]):
+                return False
+        return True
+
+    def _cleanPoint(self, point: list) -> list:
+        for i in range(len(point)):
+            point[i] = int(round(point[i], 0))
+        return point
--- Touch PoC/main.py
+++ Touch PoC/main.py
@@ -11,17 +11,25 @@ class Main():
     
     def run(self):
         try:
-            pass
+            self.controller.moveToDirect([600, 50, 250], 100)
+            self.controller.moveToDirect([600, -250, 250], 100)
+
+            for i in range(10):
+                self.controller.moveToSteps([600, 0, 250], 100)
+                self.controller.moveToSteps([600, -250, 500], 100)
+                self.controller.moveToSteps([350, 0, 500], 100)
         
         except KeyboardInterrupt:
             self.controller.stop()
         except Exception as e:
             self.controller.stop()
             print(e)
+        finally:
+            self.controller.stop()
 
 if __name__ == "__main__":
     detector = TouchDetector("COM3")
-    cobotController = CobotController(detector)
+    cobotController = CobotController(detector, 20)
 
     main = Main(cobotController)
     main.start()
