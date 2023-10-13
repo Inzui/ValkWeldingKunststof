115b8982550a13f9397612d8b5f43a333e01b4f1
Inzui <IanZuiderent@gmail.com>
2023-10-13 10:33:59 +0200

    Added run button

--- Valkwelding Kunststof Lassen/Valkwelding Kunststof Lassen/Touch PoC/MainWindow.xaml.cs
+++ Valkwelding Kunststof Lassen/Valkwelding Kunststof Lassen/Touch PoC/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace ValkWelding.Welding.Touch_PoC
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Doe dingen hier xxx
+        }
     }
 }
