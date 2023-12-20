3707907adefa951ed26dbd7b0cc90c45da97a33e
Ian Zuiderent <1004784@hr.nl>
2023-12-20 13:23:15 +0100

    Readded Debug

--- Valkwelding Kunststof Lassen/Valkwelding Kunststof Lassen/Touch PoC/Services/CobotControllerService.cs
+++ Valkwelding Kunststof Lassen/Valkwelding Kunststof Lassen/Touch PoC/Services/CobotControllerService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
